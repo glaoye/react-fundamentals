a0d6609c40a776b24c5c92cb8316e920b2acb956
Gloria Laoye <gloria.laoye@thoughtworks.com>
2022-09-21 20:48:17 +0100

    [5] Extra credit 2: Add size attribute removing dependency on className

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,13 +4,13 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-const Box = ({className='', style, ...otherProps}) => { //can split off a prop and provide it a default value
+const Box = ({size, style, ...otherProps}) => {
+  const boxSize = size ? `box--${size}` : ""
   return (
     <div
-      className={`box ${className}`.trim()}
-      //put spread last so that it is not overridden by default css
+      className={`box ${boxSize}`.trim()}
       style={{ fontStyle: 'italic', ...style}}
-      {...otherProps} // this includes props.children
+      {...otherProps}
     >
     </div>
   )
@@ -19,13 +19,13 @@ const Box = ({className='', style, ...otherProps}) => { //can split off a prop a
 function App() {
   return (
     <div>
-      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
         small lightblue box
       </Box>
-      <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
         medium pink box
       </Box>
-      <Box className="box--large" style={{backgroundColor: 'orange'}}>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
         large orange box
       </Box>
     </div>
