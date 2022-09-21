7ed204657702f806cf36d73ed921c53dcb8e7220
Gloria Laoye <gloria.laoye@thoughtworks.com>
2022-09-21 20:23:38 +0100

    [5] Extra credit 1, 1st attempt: Extracting box component and passing custom className and style as props

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,17 +4,29 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-const smallBox = <div className="box box--small" style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}>small lightblue box</div>
-const mediumBox = <div className="box box--medium" style={{backgroundColor: 'pink', fontStyle: 'italic'}}>medium pink box</div>
-const largeBox = <div className="box box--large" style={{backgroundColor: 'orange', fontStyle: 'italic'}}>large orange box</div>
+const Box = props => {
+  return (
+    <div
+      className={props.className ? `box ${props.className}` : 'box'}
+      style={{...props.style, fontStyle: 'italic'}}
+    >
+      {props.children}
+    </div>
+  )
+}
 
-//if you access an element with ${number}, you can access style with ${number}.style, which is an object in the DOM (same as in code) although a string in HTML
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box className="box--large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
     </div>
   )
 }
